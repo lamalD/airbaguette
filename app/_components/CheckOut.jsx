fc40847a8085f11c5d8edd11df3434b562a814d1
lamalD <lamal.3s@gmail.com>
2025-03-03 09:43:40 +0100

    modified:   app/_components/CheckOut.jsx

--- app/_components/CheckOut.jsx
+++ app/_components/CheckOut.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import * as React from "react"
+// import * as React from "react"
 import { addDays, format, isAfter, isBefore } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
  
