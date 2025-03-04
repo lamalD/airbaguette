75ac174645e4190c36cc491caf83552fcb8f001f
lamalD <lamal.3s@gmail.com>
2025-03-04 09:05:31 +0100

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains:['localhost', 'https://beneficial-strength-ca77e606b4.strapiapp.com']
+        domains:[
+            'localhost', 
+            'https://beneficial-strength-ca77e606b4.strapiapp.com', 
+            'https://beneficial-strength-ca77e606b4.media.strapiapp.com',
+        ]
     }
 };
 
