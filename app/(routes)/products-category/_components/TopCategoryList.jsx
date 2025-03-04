f0828a536635d1427a58cd1ac3d18e8d88595afc
lamalD <lamal.3s@gmail.com>
2025-03-04 14:07:58 +0100

    modified:   app/(routes)/products-category/_components/TopCategoryList.jsx

--- app/(routes)/products-category/_components/TopCategoryList.jsx
+++ app/(routes)/products-category/_components/TopCategoryList.jsx
@@ -6,22 +6,21 @@ function TopCategoryList({categoryList, selectedCategory}) {
 
   return (
     <div className='mt-5'>
-        
-        <div className='flex justify-center gap-4 mt-4'>
+        <div className='flex items-end justify-center gap-2 md:gap-6 px-2'>
             {categoryList.map((category, index) => (
                     <Link
-                        href={encodeURIComponent(category.name)} 
+                        href={category.name} 
                         key={index} 
-                        className={`flex flex-col lg:flex-row gap-4 p-4 items-center border-2 border-slate-200 rounded-md cursor-pointer hover:bg-slate-200 group ${selectedCategory===category.name&&'bg-slate-200'}`}
+                        className={`flex flex-col lg:flex-row gap-4 p-2 items-center border-2 border-primary rounded-md cursor-pointer hover:bg-slate-200 hover:border-secondary hover:text-primary group ${selectedCategory===category.name&&'bg-slate-200 border-secondary text-primary'}`}
                     >
                         <Image 
                             src={category.icon[0]?.url}
                             alt='icon'
                             width={100} 
                             height={100}
-                            className='group-hover:scale-125 transition-all ease-in-out'
+                            className='group-hover:scale-110 transition-all ease-in-out'
                         />
-                        <h2 className=''>{category.name}</h2>
+                        <h2 className='group-hover:scale-110 transition-all ease-in-out'>{category.name}</h2>
                     </Link>
                 ))
             }
