8643ee56411d0643c3953c01e60bb890de1b02ee
lamalD <lamal.3s@gmail.com>
2025-03-04 08:17:26 +0100

    modified:   app/_utils/GlobalApi.jsx 	modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains:['localhost']
+        domains:['localhost', 'http://beneficial-strength-ca77e606b4.strapiapp.com']
     }
 };
 
