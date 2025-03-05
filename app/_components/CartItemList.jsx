6a1071284965c36f94a993b6123c0817e339c699
lamalD <lamal.3s@gmail.com>
2025-03-05 09:02:55 +0100

    modified:   app/_components/CartItemList.jsx

--- app/_components/CartItemList.jsx
+++ app/_components/CartItemList.jsx
@@ -13,7 +13,7 @@ function CartItemList({cartItemList, onDeleteItem}) {
     <div>
         <div className='h-[500px] overflow-auto'>
             {cartItemList.map((cart, index) => (
-                <div key={index} className='flex justify-between items-center p-2 mb-5'>
+                <div key={index} className='flex justify-between items-center p-2 mb-5 border border-secondary rounded-md'>
                     <div className='flex gap-6 items-center'>
                         <Image 
                             src={cart.image} 
@@ -24,12 +24,12 @@ function CartItemList({cartItemList, onDeleteItem}) {
                         />
                         <div>
                             <h2 className='font-bold'>{cart.name}</h2>
-                            <h2>Quantity {cart.quantity}</h2>
+                            <h2>Aantal: {cart.quantity}</h2>
                             <h2 className='text-lg font-bold'>€ {cart.amount}</h2>
                         </div>
                     </div>
                     <TrashIcon 
-                        className='cursor-pointer'
+                        className='cursor-pointer text-primary hover:text-red-600'
                         onClick={() => onDeleteItem(cart.id)}
                     />
                 </div>
