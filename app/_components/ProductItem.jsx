7b777fdb0d38a2548b35766d67328ee3fac3ef63
lamalD <lamal.3s@gmail.com>
2025-03-18 17:48:58 +0100

    modified:   app/_components/ProductItem.jsx

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -37,7 +37,8 @@ function ProductItem({product}) {
   return (
     <div className='p-2 flex flex-row items-start justify-between border rounded-lg min-w-[200px] 
                     md:p-5 md:space-x-8 md:min-w-[400px] 
-                    hover:scale-105 hover:shadow-md hover:ml-3 transition-all ease-in-out scrollbar-hide'>
+                    hover:scale-105 hover:shadow-md hover:ml-3 transition-all ease-in-out scrollbar-hide'
+    >
         <Image 
             src={product.image?.url} 
             alt='image' 
@@ -45,8 +46,22 @@ function ProductItem({product}) {
             width={150} 
             height={150}
         />
-        <div className='flex flex-col justify-start items-start space-y-2 p-2 w-[75%] md:w-[50%]'>
-            <h2 className='font-bold text-sm sm:text-lg md:text-2xl text-start'>{product.name}</h2>
+        <div className='flex flex-col justify-start items-start space-y-2 p-2 w-[75%] md:w-[75%]'>
+            <h2 className='font-bold text-sm sm:text-lg md:text-2xl text-start'>
+                {product.inStock ? 
+                    (
+                        <p>{product.name}</p>
+                    ) 
+                    : 
+                    (
+                        <p>
+                            <span className="line-through text-black">{product.name}</span>
+                            <br />
+                            <span className="text-red-500">Uitverkocht</span>
+                        </p>
+                    )
+                }
+            </h2>
             {product.description ? 
                 (
                     <p className='ml-2 text-xs text-slate-300'>{formatDescription(product.description)}</p>
@@ -58,13 +73,13 @@ function ProductItem({product}) {
                 )
             }
         </div>
-        <div className='flex flex-col justify-center items-start gap-4'>
+        <div className='flex flex-col justify-center items-start gap-4 min-w-fit mt-2'>
             <div className='flex flex-row gap-3'>
                 {product.sellingPrice && <h2 className='font-bold text-lg md:text-2xl'>€ {product.sellingPrice}</h2>}
                 <h2 className={`font-bold text-lg md:text-2xl ${product.sellingPrice&&'line-through text-red-500'}`}>€ {product.mrp}</h2>
             </div>
 
-            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+            {/* <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
                 <DialogTrigger asChild>
                     <div className='flex flex-row items-center justify-center px-1.5 py-0.5 md:p-2 border rounded-lg bg-primary text-white hover:cursor-pointer hover:bg-secondary hover:text-primary ease-in-out'>
                         <Plus 
@@ -86,7 +101,46 @@ function ProductItem({product}) {
                         </DialogDescription>
                     </DialogHeader>
                 </DialogContent>
-            </Dialog>
+            </Dialog> */}
+
+            {product.inStock ? (
+                    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                        <DialogTrigger asChild>
+                            <div className='flex flex-row items-center justify-center px-1.5 py-0.5 md:p-2 border rounded-lg bg-primary text-white hover:cursor-pointer hover:bg-secondary hover:text-primary ease-in-out'>
+                                <Plus 
+                                    size={24} 
+                                    strokeWidth={3}
+                                    className='w-6'
+                                />
+                                <ShoppingBasket 
+                                    size={48}
+                                    className='w-6'
+                                />
+                            </div>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle></DialogTitle>
+                                <DialogDescription>
+                                    <ProductItemDetail product={product} onClose={closeDialog}/>
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+                ) : (
+                    <div className='flex flex-row items-center justify-center px-1.5 py-0.5 md:p-2 border rounded-lg bg-gray-300 text-gray-500 cursor-not-allowed'>
+                        <Plus 
+                            size={24} 
+                            strokeWidth={3}
+                            className='w-6'
+                        />
+                        <ShoppingBasket 
+                            size={48}
+                            className='w-6'
+                        />
+                        {/* <span className='ml-2'>Out of Stock</span> */}
+                    </div>
+                )}
         </div>
     </div>
   )
