68b57329bdd460a4d1826329aa222409f20cbbf2
lamalD <lamal.3s@gmail.com>
2025-04-21 09:01:58 +0200

    modified:   app/_components/Banner_Home.jsx 	modified:   app/api/(checkout)/create-intent/route.js

--- app/_components/Banner_Home.jsx
+++ app/_components/Banner_Home.jsx
@@ -9,7 +9,9 @@ function BannerHomepage() {
         <div className="flex mx-auto p-2 space-y-2
                         sm:px-6
                         md:space-x-8 max-w-5xl lg:px-8">
-            <p className='text-center text-sm md:text-2xl text-red-600'>Wij leveren jullie bestellingen ook op 20/04/2025 en 21/04/2025</p>
+            <p className='text-center text-sm md:text-2xl text-red-600'>
+                Bestellen kan tot 09h00 de dag zelf, ook in het weekend!
+            </p>
         </div>
     </div>
   )
